fb9e57c1a4799cb58304ba75fb1c17dd9fb8f189
giamtrot <giamtrot@hotmail.com>
2017-09-29 16:50:23 +0200

    WebApp
    Aggiunta configurazione tomcat

---
+++ web.cmd
@@ -0,0 +1,16 @@
+@echo off
+set BASE_DIR=%~dp0
+set catalina.base=%BASE_DIR%Tomcat
+REM echo %catalina.base%
+set wtp.deploy=%catalina.base%\webapps
+REM echo %wtp.deploy%
+
+set tomcat.cmd=start S:\tools\jdk1.6.0_45\bin\java.exe "-Dcatalina.base=%catalina.base%" "-Dcatalina.home=S:\tools\apache-tomcat-7.0.29" "-Dwtp.deploy=%wtp.deploy%" "-Djava.endorsed.dirs=S:\tools\apache-tomcat-7.0.29\endorsed" -Dfile.encoding=Cp1252 -classpath "S:\tools\apache-tomcat-7.0.29\bin\bootstrap.jar;S:\tools\apache-tomcat-7.0.29\bin\tomcat-juli.jar;S:\tools\jdk1.6.0_45\lib\tools.jar" org.apache.catalina.startup.Bootstrap 
+
+%tomcat.cmd% start
+explorer http://localhost:9080/varie/
+
+echo kill Tomcat?
+pause 
+
+%tomcat.cmd% stop
