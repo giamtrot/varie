ebf3f4ce60daf1e6bdeea9e89eda6611440509be
giamtrot <giamtrot@hotmail.com>
2018-09-30 09:03:39 +0200

    Versionamento

--- ext/contentScript.js
+++ ext/contentScript.js
@@ -1,4 +1,4 @@
-var version = "2018.02.25-1"
+var version = "2018.09.30-1"
 // var scriptUrl = 'http://localhost:9080/varie/lib/background.js';
 var scriptUrl = 'https://rawgit.com/giamtrot/varie/master/lib/background.js';
 // var scriptUrl = 'https://raw.githubusercontent.com/giamtrot/varie/master/lib/background.js';
