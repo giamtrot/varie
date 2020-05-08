0741c3e4618f3de10a0168a719879eddf6cda31d
giamtrot <giamtrot@hotmail.com>
2020-05-08 09:53:43 +0200

    Update after update

--- rss/AudioLibroToRss.cmd
+++ rss/AudioLibroToRss.cmd
@@ -1,6 +1,7 @@
 set BASE_DIR=%~dp0
 cd /d "%BASE_DIR%"
-REM groovy %BASE_DIR%AudioLibroToRss.groovy %1 %2
+groovy %BASE_DIR%AudioLibroToRss.groovy %1 %2
 call git add -v *
 call git commit * -m "Update after update"
 call git push -u origin master
+explorer https://raw.githubusercontent.com/giamtrot/varie/master/rss/%2.xml
