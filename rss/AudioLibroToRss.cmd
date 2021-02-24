04ca43d40ae15f92b5cfe5c0b42d64f59e98f651
giamtrot <giamtrot@hotmail.com>
2021-02-24 09:57:35 +0100

    Nuovo

--- rss/AudioLibroToRss.cmd
+++ rss/AudioLibroToRss.cmd
@@ -4,5 +4,5 @@ set GROOVY=C:\Users\giamt\Desktop\Locale\groovy-3.0.7\bin\groovy
 call %GROOVY% %BASE_DIR%AudioLibroToRss.groovy %1 %2
 call git add -v *
 call git commit * -m "Update after update"
-call git push -u origin master
+call git push https://giamtrot:PG11ctch@github.com/giamtrot/varie
 explorer https://raw.githubusercontent.com/giamtrot/varie/master/rss/%2.xml
