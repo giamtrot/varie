7154b7aacc0a2860828a619e5b5f28d438c9e992
giamtrot <giamtrot@hotmail.com>
2022-08-25 21:56:37 +0200

    new version
    new version

--- bd/content.js
+++ bd/content.js
@@ -1,5 +1,17 @@
-console.log("BD - 2020.09.23")
-Array.from(document.querySelectorAll('a')).find(el => el.textContent === 'Prezzo al Kilo piu basso').click()
-console.log("Sort done")
+console.log("BD - 2021.03.14-1")
 
+document.onload = document.onreadystatechange = function() {
+
+	if ((!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+        console.log("Starting Sort")
+		var sorter = document.querySelector("#customselect__list-0 > li:nth-child(6)")
+		if (sorter)
+			sorter.click();
+		var sorter = document.querySelector("#customselect__list-0 > li:nth-child(6)")
+		if (sorter)
+			sorter.click();
+		console.log("Sort done")
+	};
+
+};
 
