2e729c93e797d9a9ac530d0e0938ca4b80e648e0
giamtrot <giamtrot@hotmail.com>
2024-07-19 21:40:37 +0200

    .

--- civ/content.js
+++ civ/content.js
@@ -1,4 +1,5 @@
 const extId = "CIV - 2024.07.19-1"
+const WAIT = 2000
 
 document.onload = document.onreadystatechange = function() {
 
@@ -8,8 +9,27 @@ document.onload = document.onreadystatechange = function() {
 		// checkPhoto();
 	};
 	
+	loadUI()
 	document.addEventListener('keyup', doc_keyUp, false);
 };
+
+function loadUI() {
+
+	log("loadUI")
+	
+	const targetNode = document.querySelector("div.menu-extras > ul")
+	var loadButton = document.createElement("INPUT");
+	loadButton.type = "button"
+	loadButton.id ="rg-civ-load"
+	loadButton.value = "Download All"
+	loadButton.addEventListener("click", downloadAll)
+	targetNode.parentNode.insertBefore(loadButton, targetNode.nextSibling);
+
+	// targetNode.parentNode.insertBefore(document.createTextNode("&nbsp;"), targetNode.nextSibling);
+
+	log("loadUI done")
+}
+
 
 function doc_keyUp(e) {
 
@@ -20,7 +40,7 @@ function doc_keyUp(e) {
 }
 
 function log(msg){
-	console.log(extId, " - ", msg)
+	console.log(extId, " - ", new Date(), " - ", msg)
 }
 
 async function downloadAll() {
@@ -29,12 +49,17 @@ async function downloadAll() {
 	let cerca = document.querySelector('button[aria-label="Cerca"')
 	log(cerca)
 	cerca.click()
-	
-	return
+	await sleep(WAIT);
 
+	let odgTable = document.querySelectorAll("#GridOdg > div.awe-mcontent > div.awe-content.awe-tablc > div > table")[0]
+	log(odgTable)
+	let odg = Array.from(odgTable.rows).find( (r) => r.cells[1].textContent === '5' )
+	odg.click()
+	await sleep(WAIT);
+	
 	// Punti all'ordine del giorno
 	document.querySelector("a[href='#tab-puntiodg'").click()
-	await sleep(1000);
+	await sleep(WAIT);
 
 	let puntiODGRows = Array.from(document.querySelector("#GridPuntiODG > div.awe-mcontent > div.awe-content.awe-tablc > div > table > tbody").rows);
 	await clickNextPunto( puntiODGRows )
@@ -52,35 +77,46 @@ async function clickNextPunto(puntiODGRows) {
 		return;
 	}
 	
-	let row = puntiODGRows.shift();
-	row.click();
+	for (const row of puntiODGRows) {
 	
-	await sleep(1000);
-	await downloadDocumentiPunto()
+		row.click();
+		await sleep(1000);
+	
+		await downloadPunto('Allegati')
+		await downloadPunto('Documenti')
+	}
 
-	//clickNextPunto( puntiRows )
 }
 
-async function downloadDocumentiPunto() {
+async function downloadPunto(tipo) {
 
-	log("downloadDocumentiPunto")
+	log("downloadPunto: " + tipo)
 
-	let documenti = document.querySelectorAll("a[aria-label='Documenti']")[0]
+	let documenti = document.querySelectorAll("a[aria-label='" + tipo + "']")[0]
 	log(documenti)
 	documenti.click()
 
-	await sleep(1000);
+	await sleep(WAIT)
 
-	let rows = Array.from(document.querySelector("#GridDocumentiProposta > div.awe-mcontent > div.awe-content.awe-tablc > div > table > tbody").rows)
+	let rows = document.querySelector("#GridDocumentiProposta > div.awe-mcontent > div.awe-content.awe-tablc > div > table > tbody")?.rows
 	log(rows)
-	rows.forEach( (r) => {
-		console.log(r)
-	})
+	if (rows) {
+		for (const row of Array.from(rows)) {
+			log("row click")
+			row.click()
+			await sleep(WAIT);
+			log("download click")
+			let download = document.querySelector("#downloadBtn")
+			download.click()
+			await sleep(WAIT);
+		}
+	}
 
-	await sleep(10000);
+	log("chiudi wait")
+	await sleep(WAIT);
 	let chiudi = document.querySelectorAll("a[aria-label='Chiudi']")[0]
 	log(chiudi)
 	chiudi.click()
-
+	await sleep(WAIT);
 }
 
