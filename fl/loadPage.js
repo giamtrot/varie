ae48a4761fbbe9cd7ad307f2f14d17f816462fc0
giamtrot <giamtrot@hotmail.com>
2024-09-07 12:45:06 +0200

    Added album and descripton

--- fl/loadPage.js
+++ fl/loadPage.js
@@ -5,48 +5,46 @@ const LIMIT_TO = -1
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-async function loadPage(){
-	
+async function loadPage() {
+
 	log("loadPage")
 	const tables = {};
 	const cells = {};
-	
+
 	const buttons = document.querySelectorAll("a.count-overlay-button")
-	log(buttons)
-	Array.from(buttons).forEach( b => { 
-		b.click() 
+	// log(buttons)
+	Array.from(buttons).forEach(b => {
+		b.click()
 	})
 
 	await sleep(5000)
-	
+
 	const urls = document.querySelectorAll("div.photo > a.photo-link")
 	const uniqueUrls = [...new Set(Array.from(urls).map(u => u.href))];
 
 	// log(uniqueUrls);
 
-	
 	let pages = slice(uniqueUrls.slice(0, LIMIT_TO), FOR_PAGE)
-	pages.forEach( urlsForPage => loadFinalPage(urlsForPage) )
-
+	pages.forEach(urlsForPage => loadFinalPage(urlsForPage))
 }
 
 async function loadFinalPage(urls) {
 
-	log("loadFinalPage", urls)
-	
+	// log("loadFinalPage", urls)
+
 	let images = []
-	
+
 	for (const url of Array.from(urls)) {
 		images.push(await getImageInfo(url))
 	}
-	
+
 	const win = window.open("about:blank", "_blank")
 	doc = win.document
 
 	const style = doc.createElement('style');
 	style.innerHTML = getNewStyle()
 	doc.head.appendChild(style);
-	
+
 	const target = doc.createElement("div")
 	doc.body.insertBefore(target, doc.body.childNodes[0]);
 	render(target, images)
@@ -55,7 +53,7 @@ async function loadFinalPage(urls) {
 
 function fill(template, target, images) {
 	const rendered = Mustache.render(template, { images: images });
-	log(rendered)
+	// log(rendered)
 	target.innerHTML = rendered;
 }
 
@@ -69,17 +67,18 @@ function slice(array, chunkSize) {
 }
 
 async function getImageInfo(url) {
-	
-	log("getImageInfo - start", url)
+
+	// log("getImageInfo - start", url)
 	const image = {}
 	const ris = await fetch(url)
 	const html = await ris.text()
+	// log(html)
 
 	const parser = new DOMParser();
 	const doc = parser.parseFromString(html, "text/html");
 
 	const photostream = doc.querySelector("div.attribution-info > a")
-    const photostreamName = photostream?.textContent
+	const photostreamName = photostream?.textContent
 	const photostreamURL = photostream?.href
 	if (photostream == null) {
 		log("not found stream")
@@ -93,65 +92,74 @@ async function getImageInfo(url) {
 		return {}
 	}
 
+	image.url = url
+
 	// Search image if not video
 	const img = isVideo ? "" : doc.querySelectorAll("img.main-photo")[0]?.src
-	const desc = doc.querySelectorAll("meta[name='description']")[0]?.content
-    
-	image.url = url
 	image.img = img
+
 	image.photostream = { name: photostreamName, url: photostreamURL }
-	image.album = "album"
-	if (desc.length > 200) {
-		image.description = desc.substr(0, 196) + " ..."
+
+	// const desc = doc.querySelectorAll("meta[name='description']")[0]?.content
+	const desc = doc.querySelectorAll("div.title-desc-block.showFull")[0]?.innerText
+	// log(desc)
+	if (desc?.length > 200) {
+		image.description = desc.substring(0, 196) + " ..."
 		image.descriptionFull = desc
 	}
 	else {
 		image.description = desc
 		image.descriptionFull = ""
 	}
-	console.log(desc)
 
-	// // log("csrf", window?.auth?.csrf)
-	// // log("api_key", window?.YUI_config?.flickr?.api?.site_key)
-	// const csrf = window?.auth?.csrf
-	// const api_key  =window?.YUI_config?.flickr?.api?.site_key
-	// const photo_id = url.split('/')[5]
-	// const detailAPI = `https://api.flickr.com/services/rest?photo_id=${photo_id}&csrf=${csrf}&api_key=${api_key}&method=flickr.photos.getAllContexts&format=json&hermes=1&nojsoncallback=1`
-	// // log(detailAPI)
+	// log("csrf", window?.auth?.csrf)
+	// log("api_key", window?.YUI_config?.flickr?.api?.site_key)
+	const csrf = window?.auth?.csrf
+	const api_key = window?.YUI_config?.flickr?.api?.site_key
+	const photo_id = url.split('/')[5]
+	const detailAPI = `https://api.flickr.com/services/rest?photo_id=${photo_id}&csrf=${csrf}&api_key=${api_key}&method=flickr.photos.getAllContexts&format=json&hermes=1&nojsoncallback=1`
+	// log("detailAPI", detailAPI)
 
-	
+	const detailRis = await fetch(detailAPI)
+	const detailJson = await detailRis.json()
+	// log(JSON.stringify(detailJson))
 
-	// const detailRis = await fetch(detailAPI)
-	// const detailJson = await detailRis.json()
-	// // log(JSON.stringify(detailJson))
+	// // log(doc.documentElement.outerHTML)
+	// // root.auth = {"signedIn":true,"csrf":"1722138877:r29brohfedb:ddf6a30b05b02ddb3b5d3d6b16377ec2"
+	// // document.documentElement.outerHTML.indexOf("api.site_key")
+	// // root.YUI_config.flickr.api.site_key = "dead99d0ecc6f9e26ce10b525a76d210"
+	// // https://api.flickr.com/services/rest?photo_id=53885065640&method=flickr.photos.getAllContexts&csrf=1722136722%3Ahzvsu97ih4k%3Afb151e300b007170be037c7e34e0c09b&api_key=dead99d0ecc6f9e26ce10b525a76d210&format=json&hermes=1&nojsoncallback=1
 
-    // // log(doc.documentElement.outerHTML)
-    // // root.auth = {"signedIn":true,"csrf":"1722138877:r29brohfedb:ddf6a30b05b02ddb3b5d3d6b16377ec2"
-    // // document.documentElement.outerHTML.indexOf("api.site_key")
-    // // root.YUI_config.flickr.api.site_key = "dead99d0ecc6f9e26ce10b525a76d210"
-    // // https://api.flickr.com/services/rest?photo_id=53885065640&method=flickr.photos.getAllContexts&csrf=1722136722%3Ahzvsu97ih4k%3Afb151e300b007170be037c7e34e0c09b&api_key=dead99d0ecc6f9e26ce10b525a76d210&format=json&hermes=1&nojsoncallback=1
+	// const sets = JSON.stringify(detailJson.set)
+	image.album = ""
+	if (detailJson.set) {
+		log(detailJson.set)
+		const albums = detailJson.set.map(s => {
+			return { id: s.id, title: s.title, owner: s.owner.nsid }
+		})
 
-	// const album = JSON.stringify(detailJson.set)
-    // // log(desc)
-    // // log(photostream)
-
-	// createCard(cell, url, img, desc, album, photostream)
-
+		const albumUrlTemplate = "https://www.flickr.com/photos/$owner/albums/$id"
+		albums.forEach(a => a.url = albumUrlTemplate.replace("$owner", a.owner).replace("$id", a.id))
+		// log(albums)
+		const album = albums.map( a => `<a href="${a.url}">${a.title}</a>`)
+		log(album)
+		image.album = album.join("<br />")
+	} 
 	return image
-	log("getImageInfo - end", url)
+	// log("getImageInfo - end", url)
 }
 
 function render(target, images) {
 	const template = `
-<div title="{{descriptionFull}}" class="parent-container-rg">
+<div class="parent-container-rg">
     {{#images}}
-    <div class="child-container-rg">
-    <div class="photostream-rg"><a href="{{photostream.url}}">{{photostream.name}}</a></div>
-    <div class="description-rg">{{{description}}}</div>
-    <div class="album-rg">{{album}}</div>
-    <a target="_blank" href="{{url}}">
-        <img class="image-rg" src="{{img}}" />
-    </a>
+    <div title="{{descriptionFull}}" class="child-container-rg">
+		<div class="photostream-rg"><a href="{{photostream.url}}">{{photostream.name}}</a></div>
+		<div class="description-rg">{{{description}}}</div>
+		<div class="album-rg">{{{album}}}</div>
+		<a target="_blank" href="{{url}}">
+			<img class="image-rg" src="{{img}}" />
+		</a>
     </div>
     {{/images}}
 </div>
