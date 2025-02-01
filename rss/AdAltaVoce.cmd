f3375e342590134b1b339a33908f7ad4b8152523
giamtrot <giamtrot@hotmail.com>
2025-02-01 11:49:14 +0100

    update after update

--- rss/AdAltaVoce.cmd
+++ rss/AdAltaVoce.cmd
@@ -1,12 +1,9 @@
 set BASE_DIR=%~dp0
 cd /d "%BASE_DIR%"
-set GROOVY="%BASE_DIR%tools\groovy-3.0.7\bin\groovy"
-set JAVA_HOME=%BASE_DIR%tools\jdk1.8.0_272
-echo %JAVA_HOME%
 
-set URL=https://www.raiplaysound.it/audiolibri/casadaltri.json
-set NOME=casadaltri
-set PRIMO_GIORNO=01/11/2024
+set URL=https://www.raiplaysound.it/audiolibri/cuoredicane.json
+set NOME=CuoreDiCane
+set PRIMO_GIORNO=01/02/2025
 
-call %GROOVY% %BASE_DIR%AudioLibroToRss.groovy %URL% %NOME% %PRIMO_GIORNO%
+call groovy %BASE_DIR%AudioLibroToRss.groovy %URL% %NOME% %PRIMO_GIORNO%
 explorer https://raw.githubusercontent.com/giamtrot/varie/refs/heads/master/rss/%NOME%.xml
