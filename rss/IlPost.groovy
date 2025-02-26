48cd890efc5c23272ef8167b76b017645628a508
giamtrot <giamtrot@hotmail.com>
2025-02-26 12:02:30 +0100

    Il Post

--- rss/IlPost.groovy
+++ rss/IlPost.groovy
@@ -6,11 +6,18 @@ import groovy.json.JsonSlurper
 
 final RSS_DIR = /C:\Users\giamt\Documents\GitHub\varie\rss/ + "\\"
 
-def urls = ["https://www.ilpost.it/podcasts/altre-indagini/", "https://www.ilpost.it/podcasts/ci-vuole-una-scienza/", "https://www.ilpost.it/podcasts/globo/"]
+def urls = [
+    "https://www.ilpost.it/podcasts/altre-indagini/",
+    "https://www.ilpost.it/podcasts/ci-vuole-una-scienza/",
+    "https://www.ilpost.it/podcasts/globo/",
+    "https://www.ilpost.it/podcasts/morning/",
+    "https://www.ilpost.it/podcasts/orazio/"
+]
 
 def outFile = RSS_DIR + "IlPost.xml"
 
 def dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.ENGLISH)
+def dateReader = new SimpleDateFormat("dd MMM yyyy", Locale.ITALIAN)
 
 def jsonSlurper = new JsonSlurper()
 
@@ -20,78 +27,75 @@ def mapFile = new File(RSS_DIR + "IlPost.map.json")
 // Load the list from disk if it exists
 def list = []
 if (mapFile.exists()) {
-	list = jsonSlurper.parseText(mapFile.text)
+    list = jsonSlurper.parseText(mapFile.text)
 }
 list.sort { a, b -> dateFormatter.parse(a.date) <=> dateFormatter.parse(b.date) }
 
 urls.each{ inUrl->
 
-	println "podcast: $inUrl"
-	def doc = Jsoup.connect(inUrl).get()
-	// def divs = doc.select("main.container > div")
-	def divs = doc.select("main.container > div[class*=\"_episode-item_\"]")
-	divs.each{ div->
-		def link = div.select("h3 > a").first()
-		def url = link.attr("href")
-		println "episode: $url"
-		def episodeDoc = Jsoup.connect(url).get()
-		// println episode
-		def jsonData = episodeDoc.select("script").find { it.attr("type") == "application/json" }.html()
-		// println data
-		def json = jsonSlurper.parseText(jsonData)
-		def episode = json.props.pageProps.data.data.episode.data
+    println "podcast: $inUrl"
+    def doc = Jsoup.connect(inUrl).get()
+    // def divs = doc.select("main.container > div")
+    def divs = doc.select("main.container > div[class*=\"_episode-item_\"]")
+    divs.each{ div->
+        def link = div.select("h3 > a").first()
+        def url = link.attr("href")
+        println "episode: $url"
+        def episodeDoc = Jsoup.connect(url).get()
+        // println episode
+        def jsonData = episodeDoc.select("script").find { it.attr("type") == "application/json" }.html()
+        // println data
+        def json = jsonSlurper.parseText(jsonData)
+        def episode = json.props.pageProps.data.data.episode.data
 
-		if (list.find { it.src == episode.episode_raw_url[0] }) {
-			println "Already in list"
-			return
-		}
+        if (list.find { it.src == episode.episode_raw_url[0] }) {
+            println "Already in list"
+            return
+    }
 
-		def element = [
-			src: episode.episode_raw_url[0],
-		 	title: episode.title[0],
-			date: episode.date[0],
-			podcast: episode.parent.title[0]
-		]
-		if (element.title.endsWith("Seconda parte")) {
-			// println element.title
-			def dateStr = element.date
-			def date = dateFormatter.parse(dateStr)
-			date.hours += 1
-			element.date = dateFormatter.format(date)
-		}
-		list << element
-		println "$element"
-		// System.exit(0)
-	}
-
+        def element = [
+            src: episode.episode_raw_url[0],
+            title: episode.title[0],
+            date: dateReader.parse(episode.date[0]),
+            podcast: episode.parent.title[0]
+        ]
+        if (element.title.endsWith("Seconda parte")) {
+            // println element.title
+            def dateStr = element.date
+            def date = dateReader.parse(dateStr)
+            date.hours += 1
+            element.date = dateFormatter.format(date)
+        }
+        list << element
+        println "$element"
+// System.exit(0)
 }
 
+}
 
 list.sort { a, b -> dateFormatter.parse(a.date) <=> dateFormatter.parse(b.date) }
 mapFile.write(groovy.json.JsonOutput.toJson(list))
 
-
 def xmlWriter = new StringWriter()
 def xmlMarkup = new MarkupBuilder(xmlWriter)
 
-
 xmlMarkup.rss {
-	channel {
-		title("I Podcast del Post")
-		list.each{ element->
-			'item' {
-				def fullTitle = element.podcast + " - " + element.title
-// 				println urlAudio
-				title(fullTitle)
-				link(element.src)
-				description(fullTitle)
-				enclosure(type: "audio/mpeg", url:element.src)
-				// pubDate(formatter.format(element.date))
-				pubDate(element.date)
-				guid(element.src)
-			}
-		}
-	}
+    channel {
+        title("I Podcast del Post")
+        list.each{ element->
+            'item' {
+                def fullTitle = element.podcast + " - " + element.title
+                //                 println urlAudio
+                title(fullTitle)
+                link(element.src)
+                description(fullTitle)
+                enclosure(type: "audio/mpeg", url:element.src)
+                // pubDate(formatter.format(element.date))
+                pubDate(element.date)
+                guid(element.src)
+            }
+        }
+    }
 }
 
 println  xmlWriter.toString()
