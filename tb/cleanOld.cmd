030c4e37ddafa3a863844a1ce12e3554c6d3c161
giamtrot <giamtrot@hotmail.com>
2025-03-11 22:13:27 +0100

    Update cleanOld.cmd to modify file age criteria and adjust base directory

--- tb/cleanOld.cmd
+++ tb/cleanOld.cmd
@@ -1,7 +1,9 @@
-REM @echo off
+@echo off
 echo %~dpnx0
 set FND=wsl find
 
-set BASE_DIR=/mnt/c/Users/rgiammario/Downloads/tab
-%FND% %BASE_DIR% -type f -mtime +1 -print ^| wc -l
-%FND% %BASE_DIR% -type f -atime +365 -print -exec rm -f {} \;
+set BASE_DIR=/mnt/c/Users/giamt/Downloads/tab
+%FND% %BASE_DIR% -type f -cmin +120 -print ^| wc -l
+%FND% %BASE_DIR% -type f -cmin +120 -print -exec rm -f {} \;
+
+pause
