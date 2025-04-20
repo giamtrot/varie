3ac6ce9143f0ecd4114ea13811eb8dc5a6a6e6d3
giamtrot <giamtrot@hotmail.com>
2025-04-20 21:43:43 +0200

    Add colors dependency and enhance card game logic with color-coded suits

--- macchia/src/index.ts
+++ macchia/src/index.ts
@@ -1,53 +1,82 @@
+import 'colors';
+import { assert } from 'console';
+
 enum Suit {
+
+    // Hearts = "❤️",
+    // Diamonds = "♦️", 
+    // Clubs = "♣️",
+    // Spades = "♠️",
+
+    Spades = "Spades",
     Hearts = "Hearts",
     Diamonds = "Diamonds",
     Clubs = "Clubs",
-    Spades = "Spades",
+}
+
+const RED = (msg: string) => msg.red.bold;
+const BLACK = (msg: string) => msg.black.bold;
+
+const SuitInfo: Record<Suit, { color: any, index: number }> = {
+    [Suit.Spades]: { color: BLACK, index: 0 },
+    [Suit.Hearts]: { color: RED, index: 1 },
+    [Suit.Diamonds]: { color: RED, index: 2 },
+    [Suit.Clubs]: { color: BLACK, index: 3 }
 }
 
 class Card {
     value: number;
     suit: Suit;
 
+    BASE_CODE = 0x1F0A1; // Base code for playing cards
+
+    code(): number {
+        return this.BASE_CODE + this.value + (this.value <= 11 ? -1 : 0) + SuitInfo[this.suit].index * 0x10;
+    }
+
     constructor(value: number, suit: Suit) {
+        assert(value >= 1 && value <= 13, "Value must be between 1 and 13");
         this.value = value;
         this.suit = suit;
     }
 
     toString(): string {
-        let symb = "🂱"
-        return `${this.value} of ${this.suit} ${symb}`;
+        const char = String.fromCodePoint(this.code());
+        return SuitInfo[this.suit].color(char)
     }
 }
 
+class Deck {
+    cards: Card[] = [];
 
-type Deck = Card[];
-
-const deck: Deck = [];
-
-for (let m = 1; m <= 2; m++) {
-    Object.values(Suit).forEach(
-        (suit) => {
-            for (let i = 1; i <= 13; i++) {
-                const card: Card = { value: i, suit };
-                deck.push(card);
-                // console.log(card);
+    constructor() {
+        Object.values(Suit).forEach(
+            (suit) => {
+                for (let i = 1; i <= 13; i++) {
+                    const card: Card = new Card(i, suit)
+                    this.cards.push(card);
+                }
             }
-        }
-    )
-}
-
-console.log("Deck: " + JSON.stringify(deck))
-// Function to randomize the deck
-function shuffleDeck(deck: Deck): Deck {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
+        )
     }
-    return deck;
+
+    shuffle(): Deck {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+        return this;
+    }
+
+    toString(): string {
+        return this.cards.map(card => card.toString()).join("");
+    }
+
 }
 
-// Shuffle the deck
-shuffleDeck(deck);
+const deck: Deck = new Deck();
+console.log("Deck\n" + deck.shuffle().toString());
 
-console.log("Shuffled Deck: " + deck[0]);
+// console.log(`Shuffled Deck: ${deck[0]}`);
+// console.log("Shuffled Deck: " + deck[0]);
+
