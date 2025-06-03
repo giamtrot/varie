cb461354238062fb981e14043f3f0abeaff28d1d
giamtrot <giamtrot@hotmail.com>
2025-06-03 22:09:18 +0200

    Add new podcast URL for "strade-blu" to IlPost.groovy

--- rss/IlPost.groovy
+++ rss/IlPost.groovy
@@ -24,6 +24,7 @@ def urls = [
     "https://www.ilpost.it/podcasts/digital-requiem/",
     "https://www.ilpost.it/podcasts/sonar/",
     "https://www.ilpost.it/podcasts/tienimi-bordone/",
+    "https://www.ilpost.it/podcasts/strade-blu/",
 ]
 
 def outFile = RSS_DIR + "IlPost.xml"
