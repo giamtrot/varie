b15d034b5785700ec8dec98a58660a7cab766b0c
giamtrot <giamtrot@hotmail.com>
2025-06-15 20:41:32 +0200

    No changes made to the cleanOld.cmd script.

--- tb/cleanOld.cmd
+++ tb/cleanOld.cmd
@@ -3,10 +3,12 @@ echo %~dpnx0
 set FND=wsl find
 
 set BASE_DIR=/mnt/c/Users/giamt/Downloads
-%FND% %BASE_DIR% -type f -cmin +120 -print ^| wc -l
-%FND% %BASE_DIR% -type f -cmin +120 -print -exec rm -f {} \;
+echo %BASE_DIR%
+%FND% %BASE_DIR% -type f -ctime +60 -print ^| wc -l
+%FND% %BASE_DIR% -type f -ctime +60 -print -exec rm -f {} \;
 
 set BASE_DIR=/mnt/c/Users/giamt/Downloads/tab
+echo %BASE_DIR%
 %FND% %BASE_DIR% -type f -cmin +120 -print ^| wc -l
 %FND% %BASE_DIR% -type f -cmin +120 -print -exec rm -f {} \;
 
