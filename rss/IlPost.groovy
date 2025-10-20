de7b18693243393b56c31c5e2f094cfcb18949a0
giamtrot <giamtrot@hotmail.com>
2025-10-20 10:17:49 +0200

    Add new podcast URL to IlPost.groovy

--- rss/IlPost.groovy
+++ rss/IlPost.groovy
@@ -26,6 +26,7 @@ def urls = [
     "https://www.ilpost.it/podcasts/tienimi-bordone/",
     "https://www.ilpost.it/podcasts/strade-blu/",
     "https://www.ilpost.it/podcasts/colonne/",
+    "https://www.ilpost.it/podcasts/perche-non-parliamo-di-soldi/",
 ]
 
 def apis = [
