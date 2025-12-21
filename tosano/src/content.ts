7d9c8bc2a88d59267f83b1cf82171b0bf1e71148
giamtrot <giamtrot@hotmail.com>
2025-12-21 17:39:53 +0100

    Prima versione Extension TOSANO

---
+++ tosano/src/content.ts
@@ -0,0 +1,37 @@
+const EXT_ID = "TOSANO - 2024.10.31-1"
+
+log("before start", document.readyState)
+if (document.readyState != 'complete') {
+	document.onload = document.onreadystatechange = start
+}
+else {
+	start()
+}
+log("after start")
+
+//=====================================================================
+
+function start() {
+
+	log("Starting Sort")
+	// var sorter = document.querySelector("#customselect__list-0 > li:nth-child(7)") as HTMLElement
+	// log(sorter)
+	// if (sorter)
+	// 	sorter.click();
+	// sorter = document.querySelector("#customselect__list-0 > li:nth-child(7)") as HTMLElement
+	// document.querySelector("#customselect__list-0 > li:nth-child(7)")
+	// if (sorter)
+	// 	sorter.click();
+
+	if (!document.location.href.endsWith("pricePerUnit%3Aasc") &&
+		!document.location.href.endsWith("pricePerUnit:asc") &&
+		document.location.pathname.startsWith("/Categories")) {
+		document.location.href = document.location.origin + document.location.pathname + "?sort=pricePerUnit:asc"
+		log("Sort done")
+	}
+	else {
+		log("Already sorted")
+	}
+
+
+}
---
+++ tosano/src/utils.ts
@@ -0,0 +1,27 @@
+function log(...msg: (any)[]) {
+	const date = new Date();
+	const logMsg = [EXT_ID, " - ", date.toLocaleDateString(), date.toLocaleTimeString(), " - ", ...msg]
+	console.log(...logMsg)
+}
+
+async function openTab(url: string) {
+	window.postMessage({ action: "openTab", data: { url: url } }, "*");
+}
+
+function setLocalStorage(key: string, value: any) {
+	localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getLocalStorage(key: string) {
+	const value = localStorage.getItem(key);
+	if (value) {
+		return JSON.parse(value);
+	}
+	return null;
+}
+
+function initLocalStorage(key: string, initValue: any) {
+	if (!getLocalStorage(key)) {
+		setLocalStorage(key, initValue);
+	}
+}
