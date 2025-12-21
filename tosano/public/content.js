99669a308453bcc577a7a5464913f98feb576df7
giamtrot <giamtrot@hotmail.com>
2025-12-21 18:19:11 +0100

    first working version

--- tosano/public/content.js
+++ tosano/public/content.js
@@ -10,9 +10,30 @@ else {
 log("after start");
 //=====================================================================
 function start() {
+    var _a;
     log("Starting Sort");
-    var sortItem = Array.from(document.querySelectorAll(".v-list-item__title")).findLast(function (it) { return it.textContent === "Prezzo al kg crescente"; });
-    log("Found sort item:", sortItem);
-    sortItem === null || sortItem === void 0 ? void 0 : sortItem.click();
-    log("Sort done");
+    // Load sort menu
+    var sortArea = Array.from(document.querySelectorAll("span")).findLast(function (it) { return it.textContent === "Ordina per"; });
+    log("Found sort area:", sortArea);
+    var sortButton = (_a = sortArea === null || sortArea === void 0 ? void 0 : sortArea.parentNode) === null || _a === void 0 ? void 0 : _a.querySelector("div[role='button']");
+    log("Found sort button:", sortButton);
+    sortButton === null || sortButton === void 0 ? void 0 : sortButton.click();
+    var sortItem = undefined;
+    var loopCount = 0;
+    var clicker = function () {
+        if (sortItem) {
+            sortItem === null || sortItem === void 0 ? void 0 : sortItem.click();
+            log("Sort done");
+            return;
+        }
+        if (loopCount > 10) {
+            log("Sort item not found, giving up");
+            return;
+        }
+        sortItem = Array.from(document.querySelectorAll(".v-list-item__title")).findLast(function (it) { return it.textContent === "Prezzo al kg crescente"; });
+        log("Found sort item:", sortItem);
+        loopCount++;
+        setTimeout(clicker, 500);
+    };
+    setTimeout(clicker, 500);
 }
--- tosano/src/content.ts
+++ tosano/src/content.ts
@@ -14,9 +14,37 @@ log("after start")
 function start() {
 
 	log("Starting Sort")
-	const sortItem = Array.from(document.querySelectorAll(".v-list-item__title") as NodeListOf<HTMLDivElement>).findLast(it => it.textContent === "Prezzo al kg crescente")
-	log("Found sort item:", sortItem)
-	sortItem?.click()
-	log("Sort done")
+
+	// Load sort menu
+	const sortArea = Array.from(document.querySelectorAll("span")).findLast(it => it.textContent === "Ordina per");
+	log("Found sort area:", sortArea);
+
+	const sortButton = sortArea?.parentNode?.querySelector("div[role='button']") as HTMLDivElement;
+	log("Found sort button:", sortButton);
+
+	sortButton?.click()
+
+	let sortItem: HTMLDivElement | undefined = undefined;
+	let loopCount = 0;
+
+	const clicker = () => {
+		if (sortItem) {
+			sortItem?.click();
+			log("Sort done")
+			return
+		}
+
+		if (loopCount > 10) {
+			log("Sort item not found, giving up")
+			return
+		}
+
+		sortItem = Array.from(document.querySelectorAll(".v-list-item__title")).findLast(it => it.textContent === "Prezzo al kg crescente") as HTMLDivElement;
+		log("Found sort item:", sortItem);
+		loopCount++;
+		setTimeout(clicker, 500);
+	}
+
+	setTimeout(clicker, 500);
 
 }
