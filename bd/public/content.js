bcc68f0588ad14686984e18bed5bb8153c0eb8c7
giamtrot <giamtrot@hotmail.com>
2026-02-08 12:48:16 +0100

    feat: Add BBC program data, disabled program placeholder, and content scripts.

--- bd/public/content.js
+++ bd/public/content.js
@@ -11,6 +11,20 @@ log("after start");
 //=====================================================================
 function start() {
     log("Starting Sort");
+    var sortby = document.querySelector("#sortby");
+    if (sortby.value !== "Prezzo al Kg ascendente") {
+        var sorter = document.querySelector("#choices--sortby-item-choice-6");
+        log(sorter);
+        if (!sorter) {
+            log("Sorter not found");
+            return;
+        }
+        // sorter.click()
+        sorter.dispatchEvent(new Event("mousedown"));
+    }
+    else {
+        log("Already sorted");
+    }
     // var sorter = document.querySelector("#customselect__list-0 > li:nth-child(7)") as HTMLElement
     // log(sorter)
     // if (sorter)
@@ -19,13 +33,13 @@ function start() {
     // document.querySelector("#customselect__list-0 > li:nth-child(7)")
     // if (sorter)
     // 	sorter.click();
-    if (!document.location.href.endsWith("pricePerUnit%3Aasc") &&
-        !document.location.href.endsWith("pricePerUnit:asc") &&
-        document.location.pathname.startsWith("/Categories")) {
-        document.location.href = document.location.origin + document.location.pathname + "?sort=pricePerUnit:asc";
-        log("Sort done");
-    }
-    else {
-        log("Already sorted");
-    }
+    // if (!document.location.href.endsWith("pricePerUnit%3Aasc") &&
+    // 	!document.location.href.endsWith("pricePerUnit:asc") &&
+    // 	document.location.pathname.startsWith("/Categories")) {
+    // 	document.location.href = document.location.origin + document.location.pathname + "?sort=pricePerUnit:asc"
+    // 	log("Sort done")
+    // }
+    // else {
+    // 	log("Already sorted")
+    // }
 }
--- bd/src/content.ts
+++ bd/src/content.ts
@@ -14,6 +14,21 @@ log("after start")
 function start() {
 
 	log("Starting Sort")
+
+	const sortby = document.querySelector("#sortby") as HTMLSelectElement
+	if (sortby.value !== "Prezzo al Kg ascendente") {
+		const sorter = document.querySelector("#choices--sortby-item-choice-6") as HTMLDivElement
+		log(sorter)
+		if (!sorter) {
+			log("Sorter not found")
+			return
+		}
+		// sorter.click()
+		sorter.dispatchEvent(new Event("mousedown"))
+	}
+	else {
+		log("Already sorted")
+	}
 	// var sorter = document.querySelector("#customselect__list-0 > li:nth-child(7)") as HTMLElement
 	// log(sorter)
 	// if (sorter)
@@ -23,15 +38,15 @@ function start() {
 	// if (sorter)
 	// 	sorter.click();
 
-	if (!document.location.href.endsWith("pricePerUnit%3Aasc") &&
-		!document.location.href.endsWith("pricePerUnit:asc") && 
-		document.location.pathname.startsWith("/Categories")) {
-		document.location.href = document.location.origin + document.location.pathname + "?sort=pricePerUnit:asc"
-		log("Sort done")
-	}
-	else {
-		log("Already sorted")
-	}
-	
+	// if (!document.location.href.endsWith("pricePerUnit%3Aasc") &&
+	// 	!document.location.href.endsWith("pricePerUnit:asc") &&
+	// 	document.location.pathname.startsWith("/Categories")) {
+	// 	document.location.href = document.location.origin + document.location.pathname + "?sort=pricePerUnit:asc"
+	// 	log("Sort done")
+	// }
+	// else {
+	// 	log("Already sorted")
+	// }
+
 
 }
